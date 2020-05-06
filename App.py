5103e707d9eb0ff4928475a37242ef06d5af98fb
Pavan Sunny Thomas <pavansunnythomas@gmail.com>
2020-05-06 18:59:08 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,11 +1,42 @@
 import tkinter as tk
+import tkinter.filedialog as fd
+import tkinter.messagebox as msg
 import os
 
-window = tk.Tk()
+window1 = tk.Tk()
 HEIGHT = 600
 WIDTH = 800
 HOME = os.getcwd()
-frames = list(map(lambda i: (tk.PhotoImage(file=HOME+f"/Assets/fire animation/frame_{i}_delay-0.04s.gif")), range(50)))
+FILE = None
+FILEPATH = tk.StringVar()
+frames = list(
+    map(lambda i: (tk.PhotoImage(file=HOME + f"/Assets/fire animation/frame_{i}_delay-0.04s.gif")), range(50)))
+
+
+def browse():
+    global FILE, FILEPATH
+    file = fd.askopenfile(mode='r', title='select file', filetypes=[('all files', '*.*')])
+    if file is not None:
+        app.entry_bar.delete(0, len(FILEPATH.get()))
+        app.entry_bar.insert(0, file.name)
+        FILE = file.read()
+
+
+def upload():
+    global FILE, FILEPATH
+    if FILE:
+        pass  # Use the APIs accordingly
+        FILE = None
+    elif FILEPATH.get():
+        try:
+            FILE = open(file=FILEPATH.get(), mode='r')
+            pass
+            FILE = None
+        except:
+            msg.showinfo(title='Invalid File', message='Please Upload a Valid File')
+            browse()
+    else:
+        msg.showinfo(title='No File Found', message='Please Upload any File')
 
 
 class Alpha(tk.Frame):
@@ -23,19 +54,21 @@ class Alpha(tk.Frame):
         tittle = tk.Label(master=self, text=text, font=('Times', str(size)))
         tittle.place(y=y_cord, x=x_cord)
 
-    def entry(self, x_cord, y_cord, width=30, size=12):
-        entry_bar = tk.Entry(master=self, width=width, font=('Times', str(size)))
-        entry_bar.place(y=y_cord, x=x_cord)
+    def entry(self, x_cord, y_cord, width=30, size=14):
+        global FILEPATH
+        self.entry_bar = tk.Entry(master=self, width=width, font=('Times', str(size)), textvariable=FILEPATH)
+        self.entry_bar.place(y=y_cord, x=x_cord)
 
-    def button(self, text, x_cord, y_cord, width=10, height=1, size=12):
-        button = tk.Button(master=self, text=text, width=width, height=height, font=('Times', str(size)), command='')
+    def button(self, text, x_cord, y_cord, width=10, height=1, size=12, command=None):
+        button = tk.Button(master=self, text=text, width=width, height=height, font=('Times', str(size)),
+                           command=command)
         button.place(y=y_cord, x=x_cord)
 
     def folder_upload(self):
         self.label('File to upload :', 10, 250)
-        self.entry(150, 250, width=79)
-        self.button('Browse', 280, 300)
-        self.button('Upload', 400, 300)
+        self.entry(120, 250, width=74)  # for linux distros => 150, 250, width=79
+        self.button('Browse', 280, 300, command=browse)
+        self.button('Upload', 400, 300, command=upload)
 
     def updategif(self, increment, item):
         frame = frames[increment]
@@ -53,14 +86,14 @@ class Alpha(tk.Frame):
             self.after(0, self.updategif, 0, fire)
 
 
-# window properties
-window.geometry(f'{WIDTH}x{HEIGHT}')
-window.config(bg='black')
-window.resizable(width=False, height=False)
-window.title(string='Project Alpha v 0.0.0')
+# window1 properties
+window1.geometry(f'{WIDTH}x{HEIGHT}')
+window1.config(bg='black')
+window1.resizable(width=False, height=False)
+window1.title(string='Project Alpha v 0.0.0')
 
 # App working
-app = Alpha(master=window)
+app = Alpha(master=window1)
 app.label('Project Alpha', 310, 40, 20)
 app.folder_upload()
 
